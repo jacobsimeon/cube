b701804f61d7a7d7f51f4e49631ec980efc25acd
Julian Rex <julian.rex@autodesk.com>
2023-02-27 13:36:33 -0500

    WFM, but nasty hardcoded xcframework path

--- SwiftCube/Package.swift
+++ SwiftCube/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .target(name: "Cube",
                 dependencies: ["OCCube"]),
         .target(name: "OCCube",
-//                dependencies: ["cube"],
+                dependencies: ["cube"],
                 plugins: [
                     .plugin(name: "CmakeBuildPlugin",
                             package: "CmakeBuildPlugin")
@@ -30,7 +30,9 @@ let package = Package(
         ),
         .binaryTarget(
             name: "cube",
-            path: "./cube.xcframework"
+//            path: "./cube.xcframework"
+            // YUCKKKKK, but works
+            path: "../../../../../Users/rexj/Library/Developer/Xcode/DerivedData/CubeiOS-bupakfwhxitsgsgvbatjwjsqsbey/SourcePackages/plugins/swiftcube.output/OCCube/CmakeBuildPlugin/cube.xcframework"
         )
     ]
 )
